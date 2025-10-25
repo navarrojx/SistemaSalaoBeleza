cc09b85e51f171a358e59ea6bb11421f39318edf
Miguel Navarro <miguel.navarro@gmail.com>
2025-10-25 01:06:08 -0300

    #

--- Models/Agendamento.cs
+++ Models/Agendamento.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using AngelaAraujo.Models;
 using System.Collections.Generic;
